925ae057c4dd5d1644d509de26656388bcdb009d
imaimai17468 <tosiki_801_life@outlook.jp>
2023-09-11 11:38:28 +0900

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Heading, Box, Input, Flex } from "@kuma-ui/core";
+
 export default function Home() {
   return (
-    <main>
-      <h1>GitHub Contribution Battle</h1>
+    <main className="bg-gray-100 h-screen">
+      <Box p={8}>
+        <Heading className="md:text-2xl text-xl">
+          GitHub Contribution Battle
+        </Heading>
+        <Flex justifyContent="center" my={48}>
+          <Input
+            placeholder="Enter a GitHub username (hoge, huga, ...)"
+            p={4}
+            className="w-4/5 md:w-1/2 rounded-md border-gray-200 border"
+          />
+        </Flex>
+      </Box>
     </main>
-  )
+  );
 }
